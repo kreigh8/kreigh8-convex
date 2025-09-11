88082487b003b7eb96a62ead44f7c8d50950666c
Kreigh Hirschy <khirschy@kreigh8.com>
2025-09-11 17:29:49 -0400

    title change

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,12 @@ export default function Home() {
   return (
     <>
       <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
-        Convex + Next.js + Clerk
+        kreigh8
         <UserButton />
       </header>
       <main className="p-8 flex flex-col gap-8">
         <h1 className="text-4xl font-bold text-center">
-          Convex + Next.js + Clerk
+          kreigh8
         </h1>
         <Authenticated>
           <Content />
