29239de89e2958beab3236e18e36417a1d15f5ee
Kreigh Hirschy <khirschy@kreigh8.com>
2025-09-12 14:13:13 -0400

    sign in redirect

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { useAuth } from "@clerk/nextjs";
+
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/server"]);
+// const isProtectedRoute = createRouteMatcher(["/server"]);
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
 });
 
 export const config = {
