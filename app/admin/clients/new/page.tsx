acf4ebc5642ed4d7e8c3d64219eca4fcd04e7ef0
Kreigh Hirschy <khirschy@kreigh8.com>
2025-09-16 23:04:00 -0400

    remove unused code

--- app/admin/clients/new/page.tsx
+++ app/admin/clients/new/page.tsx
@@ -1,4 +1,3 @@
-import Breadcrumbs from '@/components/Breadcrumbs'
 import ClientForm from '@/components/forms/ClientForm'
 
 export default function AddClientPage() {
--- components/Login.tsx
+++ components/Login.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Authenticated, Unauthenticated } from 'convex/react'
-import { LogOut, Settings, User } from 'lucide-react'
+import { LogOut, Settings } from 'lucide-react'
 import SignInButton from './SignInButton'
 import Link from 'next/link'
 import { Button } from './ui/button'
